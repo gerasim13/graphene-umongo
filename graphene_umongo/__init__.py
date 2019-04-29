e806b93d072bd7679bc5e0bff61c748c159c0b41
Павел Литвиненко <digitaldistortion@ya.ru>
2019-04-30 00:36:53 +0300

    add version

--- graphene_umongo/__init__.py
+++ graphene_umongo/__init__.py
@@ -1,6 +1,7 @@
-from .types import UMongoObjectType
 from .querysets import UMongoFindQueryset, UMongoAggregationQueryset
+from .types import UMongoObjectType
 
+__version__ = "0.0.1"
 
 __all__ = [
     "UMongoObjectType",
